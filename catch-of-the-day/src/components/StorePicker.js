e832ad17aa3356ed70ebed307849e16fde9b71df
Shivam Ramdhani <shivamr@bsf.io>
2021-08-08 13:30:32 +0530

    new: Header & App setup.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,7 +2,13 @@ import React from 'react';
 
 class StorePicker extends React.Component {
     render() {
-        return <p>Its a store picker</p>
+        return (
+            <form action="" className="store-selector">
+                <h2>Please Enter a Store.</h2>
+                <input type="text" name="" id="" required placeholder="Store Name" />
+                <button type="submit" >Visit Store</button>
+            </form>
+        );
     }
 }
 
