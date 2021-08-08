bf78a05de0cb0e5ae20249111d392fc67ed171c7
Shivam Ramdhani <shivamr@bsf.io>
2021-08-08 15:56:50 +0530

    importing helper.js function.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
     render() {
         return (
             <form action="" className="store-selector">
                 <h2>Please Enter a Store.</h2>
-                <input type="text" name="" id="" required placeholder="Store Name" />
+                <input type="text" name="" id="" required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit" >Visit Store</button>
             </form>
         );
