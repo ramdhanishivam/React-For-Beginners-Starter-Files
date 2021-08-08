8c39141c9f969afa35ee397cd4b3affcc1c24f85
Shivam Ramdhani <shivamr@bsf.io>
2021-08-08 22:30:09 +0530

    createRef for input field created.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,17 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+    goToStore = event => {
+        event.preventDefault();
+        console.log(this.myInput);
+    }
+
     render() {
         return (
-            <form action="" className="store-selector">
+            <form action="" className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter a Store.</h2>
-                <input type="text" name="" id="" required placeholder="Store Name" defaultValue={getFunName()} />
+                <input type="text" name="" id="" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit" >Visit Store</button>
             </form>
         );
