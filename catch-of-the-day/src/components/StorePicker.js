d986a0ac12912ab82af0ee12d311d5cf4f3b0d16
Shivam Ramdhani <shivamr@bsf.io>
2021-08-12 09:14:19 +0530

    goToStore property updated - handling input.

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -5,14 +5,15 @@ class StorePicker extends React.Component {
     myInput = React.createRef();
     goToStore = event => {
         event.preventDefault();
-        console.log(this.myInput);
+        const storeName = this.myInput.current.value;
+        this.props.history.push(`/store/${storeName}`);
     }
 
     render() {
         return (
             <form action="" className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter a Store.</h2>
-                <input type="text" name="" id="" ref={this.myInput} required placeholder="Store Name" defaultValue={getFunName()} />
+                <input type="text" ref={this.myInput} require placeholder="Store Name" defaultValue={getFunName()} />
                 <button type="submit" >Visit Store</button>
             </form>
         );
